117792759960246c0309173e746cbe6944e0d633
Samit Koyom <samitkoyom@gmail.com>
2025-02-17 23:51:41 +0700

    add staic file

--- dekend/settings.py
+++ dekend/settings.py
@@ -194,6 +194,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# For dev mode
+# STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
+
+# For prod mode
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
